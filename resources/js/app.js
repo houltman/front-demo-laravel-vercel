be469d2c1af29d26115e14119b3472a045c9dfc8
houltman <houltman_gonzalez@hotmail.com>
2024-09-18 18:32:46 -0400

    add feature vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import { createApp } from 'vue';
+
+// Importa tu componente
+import ExampleComponent from './components/ExampleComponent.vue';
+
+// Crea la instancia de Vue
+const app = createApp({});
+
+// Registra el componente
+app.component('example-component', ExampleComponent);
+
+// Monta la aplicación
+app.mount('#app');
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Hola desde Vue!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ExampleComponent'
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: blue;
+  }
+  </style>
+  
