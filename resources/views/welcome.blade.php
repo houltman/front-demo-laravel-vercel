cd01c2f90db28ad69f614183928855fddd38b6cc
houltman <houltman_gonzalez@hotmail.com>
2024-09-18 18:00:15 -0400

    fix loading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,8 @@
         </style>
     </head>
     <body class="antialiased">
+    <h1 >Deploy test ok </h1>
+    <!--
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -128,5 +130,6 @@
                 </div>
             </div>
         </div>
+        -->
     </body>
 </html>
