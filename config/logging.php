aa98417190a22994c92303e1ee31f52b70251f8f
houltman <houltman_gonzalez@hotmail.com>
2024-09-18 16:23:22 -0400

    fix logging

--- config/logging.php
+++ config/logging.php
@@ -56,7 +56,8 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => env('LOG_PATH', '/tmp/laravel.log'),
+            //'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
